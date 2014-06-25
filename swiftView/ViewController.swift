084230d343302def3a2b6cd91ba94f7c894526a0
mingcute <bestdearlove@gmail.com>
2014-06-25 16:01:10 +0800

    Initial Commit

---
+++ swiftView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftView
+//
+//  Created by 李健銘 on 2014/6/25.
+//  Copyright (c) 2014年 Takobear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
